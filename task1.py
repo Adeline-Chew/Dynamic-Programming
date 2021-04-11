48f73d8de3640e81588f70d05152543db8355407
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2021-04-11 22:53:22 +0800

    11/4/2021

--- task1.py
+++ task1.py
@@ -12,18 +12,19 @@ def best_schedule(weekly_income, competitions):
         weekly_income[i] = (i, i, weekly_income[i])
     competitions.extend(weekly_income)
     competitions.sort(key=take_ending)
+    # competitions.sort(key=lambda x:x[1])
     sort_profit(competitions)
     print(competitions)
     if len(memo) > 1:
         memo[1] = competitions[0][2]
     td_best_schedule(competitions, memo)
+    print(memo[-1])
     return memo[-1]
-    # return memo[-1]
     
 def td_best_schedule(lst, memo):
     n = len(lst)
     for i in range(1, n + 1):
-        include = binary_search(lst, i - 1)
+        include = another_binary(lst, i - 1)
         current_profit = memo[include[1] + 1] + lst[i - 1][2] if include[0] else lst[i - 1][2]
         # current_profit = memo[include[1] + 1] + lst[i - 1][2]
         memo[i] = max(current_profit, memo[i - 1])
@@ -32,7 +33,7 @@ def td_best_schedule(lst, memo):
 def take_ending(item):
     return item[1]
 
-# Start time of current_job = x, find job that ends at x - 1
+# Start time of current_job = x, find max_profit job that ends at x - 1
 def binary_search(lst, current_index):
     current_start_time = lst[current_index][0]
     lo = 0
@@ -51,6 +52,23 @@ def binary_search(lst, current_index):
             hi = mid - 1
     return False, 0
 
+def another_binary(lst, cur_index):
+    current_start_time = lst[cur_index][0]
+    lo = 0
+    hi = cur_index - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        mid_end_time = lst[mid][1]
+        next_end_time = lst[mid + 1][1]
+        if mid_end_time == current_start_time - 1 and next_end_time >= current_start_time:
+            return True, mid
+        elif current_start_time <= mid_end_time:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return False, 0
+            
+
 week = [3, 7, 2, 1, 8, 4, 5]
 competitions = [(1,3,15),(2,2,8),(0,4,30),(3,5,19)]
 # print(best_schedule(week, competitions) == 42)
@@ -58,3 +76,14 @@ competitions = [(1,3,15),(2,2,8),(0,4,30),(3,5,19)]
 # print(best_schedule([3, 7, 18, 1, 8, 4, 5], [(1, 3, 15), (2, 2, 8), (0, 4, 30), (3, 5, 19), (5, 6, 14)]) == 52)
 # print(best_schedule([3, 7, 2, 1, 8, 4, 5], [(1, 3, 15), (2, 2, 8), (3, 4, 30), (3, 5, 19), (5, 6, 1)]) == 57)
 # print(best_schedule([3, 7, 2, 5, 8, 4, 5], [(1, 3, 15), (1, 2, 14), (0, 4, 30), (3, 5, 19)]  ) == 41)
+weekly_income = [3, 7, 2, 1, 8, 4, 5]
+competitions = [(0, 6, 1000)]
+competitions1 = [(0, 5, 1000)]
+print(best_schedule(weekly_income, competitions) == 1000)
+print(best_schedule([3, 7, 2, 1, 8, 4, 5], [(0, 5, 1000)]) == 1005)
+weekly_income = [4, 6, 3, 2]
+competitions = [(1, 3, 10), (2, 3, 10)]
+print(best_schedule(weekly_income, competitions) == 20)
+weekly_income = [1, 2, 3, 4, 5]
+competitions = [(1, 3, 10), (1, 2, 15)]
+print(best_schedule(weekly_income, competitions) == 25)
--- task2.py
+++ task2.py
@@ -1,2 +1,53 @@
-def best_itinerary():
-    pass
+# multiple memo
+
+def calc_profits(day, max_days, profit, quarantine_time, city, need_quarantine=False):
+    earned = 0
+    date_until = day + max_days + 1
+    if need_quarantine:
+        day += quarantine_time[city] + 1
+    else:
+        max_days += 1 # include travelling day
+        date_until = day + max_days + 1
+    while day < len(profit) and day <= date_until:
+        earned += profit[day][city]
+        day += 1
+    return earned
+
+
+def best_itinerary(profit, quarantine_time, home):
+    cities = len(quarantine_time)
+    days = len(profit)
+    memo = [[0] * (cities + 2) for _ in range(days + 1)]
+    quarantine_time = [-1] + quarantine_time + [-1]
+    day = 0
+    current_city = home + 1
+    while day < days: # days or days + 1?
+        max_days = max(quarantine_time[current_city + 1], quarantine_time[current_city - 1])
+        stay = calc_profits(day, max_days, profit, quarantine_time, current_city, False)
+        left_city = calc_profits(day + 1, max_days, profit, quarantine_time, current_city - 1, True)
+        right_city = calc_profits(day + 1, max_days, profit, quarantine_time, current_city + 1, True)
+        max_profit = max(stay, left_city, right_city)
+        if max_profit == stay:
+            memo[max_days][current_city] = stay
+        elif max_profit == left_city:
+            memo[max_days][current_city - 1] = left_city
+            current_city -= 1
+        else:
+            memo[max_days][current_city + 1] = right_city
+            current_city += 1
+        day += max_days    
+    return memo[days][current_city]
+
+
+profit = [
+            [6, 9, 7, 5, 9],
+            [4, 7, 3, 10, 9],
+            [7, 5, 4, 2, 8],
+            [2, 7, 10, 9, 5],
+            [2, 5, 2, 6, 1],
+            [4, 9, 4, 10, 6],
+            [2, 2, 4, 8, 7],
+            [4, 10, 2, 7, 4]
+        ]
+quarantine_time = [3, 1, 1, 1, 1]
+print(best_itinerary(profit, quarantine_time, 0))
